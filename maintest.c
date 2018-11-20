1b7a02751c3f5a9f355c891fd0a9073d6ef34193
Tristan MONICHON <trmonich@e1r5p10.42.fr>
2018-11-20 10:40:56 +0100

    surveillew les change;ent

--- maintest.c
+++ maintest.c
@@ -6,7 +6,7 @@
 /*   By: trmonich <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 16:13:19 by trmonich          #+#    #+#             */
-/*   Updated: 2018/11/19 16:11:57 by trmonich         ###   ########.fr       */
+/*   Updated: 2018/11/19 18:48:22 by trmonich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,13 @@
 int	main()
 {
 	int	fd;
-	char **str;
+	char *str;
 	int i;
 
 	i = 0;
-	str = malloc(sizeof(*str) * 100);
 	fd = open("test.txt", O_RDONLY);
-	ft_putnbr(get_next_line(fd, &str[0]));
-	ft_putchar(10);
-	ft_putendl(str[0]);
-	ft_putnbr(get_next_line(fd, &str[1]));
-	ft_putchar(10);
-	ft_putendl(str[1]);
-	ft_putnbr(get_next_line(fd, &str[2]));
-	ft_putchar(10);
-	ft_putendl(str[2]);
-	ft_putnbr(get_next_line(fd, &str[3]));
-	ft_putchar(10);
-	ft_putendl(str[3]);
-	ft_putnbr(get_next_line(fd, &str[4]));
-	ft_putchar(10);
-	ft_putendl(str[4]);
-	ft_putnbr(get_next_line(fd, &str[5]));
-	ft_putchar(10);
-	ft_putendl(str[5]);
+	while (get_next_line(fd, &str))
+		ft_putendl(str);
 	return (0);
 }
 
